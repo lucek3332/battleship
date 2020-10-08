271c48e71e63871e421a8b1f02b26760f314f7c2
lucek3332 <jakubszulgit@gmail.com>
2020-10-08 21:10:50 +0200

    Modifying classes Ship, Board and Field.
    Modifying Ship class for handling correct placing ships on the board. Putting constraints (collision with other ships, lying out of the board). Modifying Board class for allowing to reset the board. Adding methods ship_fields and surrounding_ship_fields to Board class (in future need to move these methods to Ship class). Adding Button class to client.py. Modifying client.py for testing introduced changes.

--- classes.py
+++ classes.py
@@ -25,6 +25,7 @@ class Field:
         self.active = True
         self.hitted = False
         self.ship = False
+        self.shipAvaiable = True
         self.sinked = False
 
     def draw(self, win):
@@ -35,6 +36,8 @@ class Field:
             pygame.draw.line(win, self.lines_color, (self.x, self.y + self.height), (self.x + self.width, self.y))
         elif self.hitted:
             pygame.draw.circle(win, self.circle_color, (round(self.x + self.width/2), round(self.y + self.height/2)), 5)
+        if not self.shipAvaiable:
+            pygame.draw.rect(win, (0, 255, 0), self.rect)
 
     def click(self):
         pos = pygame.mouse.get_pos()
@@ -47,6 +50,16 @@ class Field:
                 self.active = False
                 return True
         return False
+
+    def isShipPoss(self, ship):
+        if ship.position == "horizontal":
+            if self.col + ship.mast <= 10:
+                return True
+            return False
+        else:
+            if self.row + ship.mast <= 10:
+                return True
+            return False
 
     def __repr__(self):
         return "Field ({}, {})".format(self.col, self.row)
@@ -64,8 +77,73 @@ class Board:
     def draw(self, win):
         for row in self.fields:
             for field in row:
-
                 field.draw(win)
+
+    def reset_board(self, ships):
+        for row in self.fields:
+            for field in row:
+                field.ship = False
+                field.shipAvaiable = True
+        for ship in ships:
+            ship.reset()
+
+    # add to Ship class later, remove from Board, algorithm need to be improved
+    def surrounding_ship_fields(self, field, ship):
+        ship.surrounding = []
+        if ship.position == "horizontal":
+            if field.row == 0:
+                if field.col == 0:
+                    fields = [(r, c) for c in range(ship.mast + 1) for r in range(field.row + 2)]
+                elif field.col + ship.mast == 10:
+                    fields = [(r, c) for c in range(field.col - 1, 10) for r in range(field.row + 2)]
+                else:
+                    fields = [(r, c) for c in range(field.col - 1, field.col + ship.mast + 1) for r in range(field.row + 2)]
+            elif field.row == 9:
+                if field.col == 0:
+                    fields = [(r, c) for c in range(ship.mast + 1) for r in range(8, 10)]
+                elif field.col + ship.mast == 10:
+                    fields = [(r, c) for c in range(field.col - 1, 10) for r in range(8, 10)]
+                else:
+                    fields = [(r, c) for c in range(field.col - 1, field.col + ship.mast + 1) for r in range(8, 10)]
+            elif field.col == 0:
+                fields = [(r, c) for c in range(ship.mast + 1) for r in range(field.row - 1, field.row + 2)]
+            elif field.col + ship.mast == 10:
+                fields = [(r, c) for c in range(field.col - 1, 10) for r in range(field.row - 1, field.row + 2)]
+            else:
+                fields = [(r, c) for c in range(field.col - 1, field.col + ship.mast + 1) for r in range(field.row - 1, field.row + 2)]
+
+        else:
+            if field.row == 0:
+                if field.col == 0:
+                    fields = [(r, c) for c in range(2) for r in range(ship.mast + 1)]
+                elif field.col == 9:
+                    fields = [(r, c) for c in range(8, 10) for r in range(ship.mast + 1)]
+                else:
+                    fields = [(r, c) for c in range(field.col - 1, field.col + 2) for r in range(ship.mast + 1)]
+            elif field.row + ship.mast == 10:
+                if field.col == 0:
+                    fields = [(r, c) for c in range(2) for r in range(field.row - 1, 10)]
+                elif field.col == 9:
+                    fields = [(r, c) for c in range(8, 10) for r in range(field.row - 1, 10)]
+                else:
+                    fields = [(r, c) for c in range(field.col - 1, field.col + 2) for r in range(field.row - 1, 10)]
+            elif field.col == 0:
+                fields = [(r, c) for c in range(2) for r in range(field.row - 1, field.row + ship.mast + 1)]
+            elif field.col == 9:
+                fields = [(r, c) for c in range(8, 10) for r in range(field.row - 1, field.row + ship.mast + 1)]
+            else:
+                fields = [(r, c) for c in range(field.col - 1, field.col + 2) for r in range(field.row - 1, field.row + ship.mast + 1)]
+
+        ship.surrounding = fields
+
+    # add to Ship class later, remove from Board
+    def ship_fields(self, field, ship):
+        ship.fields = []
+        if ship.position == "horizontal":
+            fields = [(field.row, c) for c in range(field.col, field.col + ship.mast)]
+        else:
+            fields = [(r, field.col) for r in range(field.row, field.row + ship.mast)]
+        ship.fields = fields
 
 
 class Ship:
@@ -76,14 +154,25 @@ class Ship:
     def __init__(self, mast, x, y):
         self.x = x
         self.y = y
+        self.start_x = x
+        self.start_y = y
         self.mast = mast
         self.rect = (self.x, self.y, self.mast * self.width, self.height)
         self.draging = False
+        self.position = "horizontal"
+        self.placed = False
+        self.fields = []
+        self.surrounding = []
+        self.sinked = False
 
     def draw(self, win):
-        for i in range(self.mast):
-            win.blit(self.image, (self.x + i * self.width, self.y))
-            pygame.draw.rect(win, (255, 0, 0), self.rect, 4)
+        if self.position == "horizontal":
+            for i in range(self.mast):
+                win.blit(self.image, (self.x + i * self.width, self.y))
+        else:
+            for i in range(self.mast):
+                win.blit(self.image, (self.x, self.y + i * self.height))
+        pygame.draw.rect(win, (255, 0, 0), self.rect, 4)
 
     def click(self):
         pos = pygame.mouse.get_pos()
@@ -96,8 +185,53 @@ class Ship:
         pos = pygame.mouse.get_pos()
         self.x = pos[0]
         self.y = pos[1]
-        self.rect = (self.x, self.y, self.mast * self.width, self.height)
+        self.update()
 
-    def drop(self):
-        pos = pygame.mouse.get_pos()
+    def reset(self):
+        self.x = self.start_x
+        self.y = self.start_y
+        self.position = "horizontal"
+        self.surrounding = []
+        self.update()
 
+    def drop(self, board):
+        for row in board.fields:
+            for field in row:
+                if field.x <= self.x <= field.x + field.width:
+                    if field.y <= self.y <= field.y + field.height:
+                        if field.isShipPoss(self):
+                            board.surrounding_ship_fields(field, self)
+                            board.ship_fields(field, self)
+                            for f in self.fields:
+                                if not board.fields[f[0]][f[1]].shipAvaiable:
+                                    self.reset()
+                                    return False
+                            for f in self.surrounding:
+                                if board.fields[f[0]][f[1]].ship:
+                                    self.reset()
+                                    return False
+                            for f in self.fields:
+                                board.fields[f[0]][f[1]].ship = True
+                            for f in self.surrounding:
+                                board.fields[f[0]][f[1]].shipAvaiable = False
+                            self.x = field.x
+                            self.y = field.y
+                            self.placed = True
+                            self.update()
+                            return True
+
+        self.reset()
+        return False
+
+    def update(self):
+        if self.position == "horizontal":
+            self.rect = (self.x, self.y, self.mast * self.width, self.height)
+        else:
+            self.rect = (self.x, self.y, self.width, self.mast * self.height)
+
+    def rotate(self):
+        if self.position == "horizontal":
+            self.position = "vertical"
+        else:
+            self.position = "horizontal"
+        self.update()
--- client.py
+++ client.py
@@ -13,21 +13,52 @@ screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Battleship - online game")
 
 
-def redrawWindow(win, my_board, enemy_board, ships):
+class Button:
+    color_bg = (255, 0, 0)
+    color_txt = (255, 255, 255)
+
+    def __init__(self, text, x, y, width, height):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color_bg, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont("comicsans", 30)
+        txt = font.render(self.text, True, self.color_txt)
+        win.blit(txt, (round(self.x + self.width/2 - txt.get_width()/2), round(self.y + self.height/2 - txt.get_height()/2)))
+
+    def click(self):
+        pos = pygame.mouse.get_pos()
+        if self.x <= pos[0] <= self.x + self.width:
+            if self.y <= pos[1] <= self.y + self.height:
+                return True
+        return False
+
+
+def redrawWindow(win, my_board, enemy_board, ships, buttons):
     win.fill((162, 213, 252))
     my_board.draw(win)
     enemy_board.draw(win)
     for ship in ships:
         ship.draw(win)
+    for button in buttons:
+        button.draw(win)
     pygame.display.update()
 
+
 def main():
+    buttons = []
     run = True
     b = Board()
     b2 = Board(550, 100)
-    ships = [Ship(4, 500, 600), Ship(3, 500, 660)]
+    ships = [Ship(4, 100, 530), Ship(3, 100, 590), Ship(3, 250, 590), Ship(2, 100, 650), Ship(2, 200, 650), Ship(2, 300, 650), Ship(1, 100, 710), Ship(1, 160, 710), Ship(1, 220, 710), Ship(1, 280, 710)]
+    reset_btn = Button("RESET", 350, 520, 100, 40)
+    buttons.append(reset_btn)
     while run:
-        redrawWindow(screen, b, b2, ships)
+        redrawWindow(screen, b, b2, ships, buttons)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -37,11 +68,19 @@ def main():
                     for field in row:
                         if field.active and field.click():
                             print("hitted")
+            if event.type == pygame.MOUSEBUTTONUP and reset_btn.click():
+                b.reset_board(ships)
             for ship in ships:
-                if event.type == pygame.MOUSEBUTTONUP and ship.click():
+                if event.type == pygame.MOUSEBUTTONUP and ship.click() and not ship.placed:
+                    if ship.draging:
+                        ship.drop(b)
                     ship.draging = not ship.draging
-                elif event.type == pygame.MOUSEMOTION and ship.draging == True:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE and ship.draging is True:
+                        ship.rotate()
+                elif event.type == pygame.MOUSEMOTION and ship.draging is True:
                     ship.drag()
+
 
 
 main()
